a1c2878f60dfded273a13bd682ffcd91db85e36f
Damian Simon Peter <simon.peter@andela.com>
2017-03-02 15:15:05 +0100

    Fixed all TODOs by providing the information required - Add .rspec for rspec helper

--- spec/white_box_testing_examples_spec.rb
+++ spec/white_box_testing_examples_spec.rb
@@ -5,7 +5,7 @@ describe WhiteBoxTestingExamples do
     expect(WhiteBoxTestingExamples::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "the truth" do
+    expect(true).to be_truthy
   end
 end
