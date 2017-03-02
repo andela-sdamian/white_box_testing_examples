0a99c0ab9ff2853fe7649782124ebb71fd77e403
Damian Simon Peter <simon.peter@andela.com>
2017-03-02 16:28:38 +0100

    Fix indentation issues

--- lib/white_box_testing_examples/path_coverage.rb
+++ lib/white_box_testing_examples/path_coverage.rb
@@ -8,6 +8,7 @@ module WhiteBoxTestingExamples
       def initialize(direct_labor_cost)
         raise FloatNotGiven unless direct_labor_cost.is_a? Float
         raise ValueCannotBeLessThanOne if direct_labor_cost < 1
+
         @direct_labor_cost = direct_labor_cost
       end
     end
